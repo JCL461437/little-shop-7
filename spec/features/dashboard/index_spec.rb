1e2ff455a090daba85286da91a72cba5f770c1ca
Garrett Bowman <garrettbowman248@gmail.com>
2024-05-29 19:37:58 -0500

    User story 4

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -30,5 +30,18 @@ RSpec.describe "Merchant Dashboard Show Page" do
                 # add expects to check for top 5 customers
             end
         end
+        scenario "I see a div for items ready to ship" do
+            visit merchant_dashboard_index_path(@merchant)
+
+            within("#items_to_ship") do
+                expect(page).to have_content("Items Ready To Ship")
+                expect(page).to have_content("Item Name")
+                expect(page).to have_content("Invoice Number")
+                expect(page).to have_content("Date")
+                # add data to check in table
+                
+            end
+        end
+
     end
 end
