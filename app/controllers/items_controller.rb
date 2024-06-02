ede27503d25215831d2c3652219348a67d0abf2a
Lance Nelson <lancen04@gmail.com>
2024-06-02 17:20:07 -0400

    working on the routing with updating the items show page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,23 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(item_params[:id])
+  end
+
+  def update
+    item = Item.find(item_params[:id])
+    if item.update(item_params)
+      redirect_to "/items/#{item.id}"
+      flash[:alert] = "Item Information Successfully Updated!"
+    else
+      redirect_to "/items/#{item.id}/edit"
+      flash[:alert] = "Error: Please Fill in All Fields"
+    end
+  end
+
+  def item_params
+    params.permit(:id, :name, :description, :unit_price)
+  end
 end
