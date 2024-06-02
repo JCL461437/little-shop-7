b3f575860f43f1c4bef2dbc9c3ab0cfea685c0e9
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-06-02 16:32:52 -0600

    write test

---
+++ spec/features/admin/merchants/index_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "admin merchants index" do
+
+  describe "As an admin when I visit the admin merchants index (/admin/merchants)" do
+    it "I see the name of each merchant in the system" do
+      merchants = FactoryBot.create_list(:merchant, 10)
+
+      visit admin_merchants_path
+
+      merchants.each do |merchant|
+        expect(page).to have_content(merchant.name)
+    end
+  end
+end
