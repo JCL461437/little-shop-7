3d26d0c74c0795de4bb8caba2091465a3a586378
Jan Lehigh <janlehigh461437@gmail.com>
2024-06-03 15:06:19 -0600

    Add instance variables for enabled and disabled merchants.

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,6 +1,8 @@
 class Admin::MerchantsController < ApplicationController
   def index
     @merchants = Merchant.all
+    @enabled_merchants = enabled_merchants
+    @disabled_merchants = disabled_merchants
   end
 
   def show
