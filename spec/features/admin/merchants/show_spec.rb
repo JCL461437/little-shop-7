52613c42fb349bed0726d4a84a6546a3202e55f4
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-06-03 13:39:42 -0600

    write test

---
+++ spec/features/admin/merchants/show_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe "admin merchants show" do
+  describe "As an admin when I visit /admin/merchants/:merchant_id" do
+    describe "display" do
+      it "I see a link to update the merchant's information" do
+        merchant_1 = Merchant.create!(name: "Acme")
+        
+        visit admin_merchant_path(merchant_1)
+
+        expect(page).to have_link("Update Merchant")
+      end
+    
+      it "the link takes me to a page to edit this merchant and I see a form filled in with the merchant info" do
+        merchant_1 = Merchant.create!(name: "Acme")
+        
+        visit admin_merchant_path(merchant_1)
+        click_link("Update Merchant")
+
+        expect(current_path).to eq(edit_admin_merchant_path(merchant_1))
+        expect(page).to have_content("Acme")
+      end
+
+      it "when I update the form and click 'submit' I am redirected to /admin/merchants/:merchant_id and see updated info and flash message of successful update" do
+        merchant_1 = Merchant.create!(name: "Acme")
+        
+        visit admin_merchant_path(merchant_1)
+        click_link("Update Merchant")
+
+        fill_in "Name", with: "Acme Inc."
+        click_button "submit"
+
+        expect(current_path).to eq(admin_merchant_path(merchant_1))
+        expect(page).to have_content("Acme Inc.")
+      end
+    end
+
+  end
+
+end
