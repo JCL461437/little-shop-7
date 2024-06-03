584dcf538c3376c33ff941866f6bf85cc6e1892a
Jan Lehigh <janlehigh461437@gmail.com>
2024-06-03 16:53:53 -0600

    Add tests for new view functionality including button clicking, changing of status between enabled and disabled, and displaying merchants in the correct sections.

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -38,21 +38,37 @@ RSpec.describe "admin merchants index" do
       # Then I am redirected back to the admin merchants index
       # And I see that the merchant's status has changed
       it "Displays a button that can change a merchant's status" do
-        merchant_1 = Merchant.create!(name: "Mans Machines", status: 1)
-        merchant_2 = Merchant.create!(name: "Guys Gizzmos", status: 1)
-        merchant_3 = Merchant.create!(name: "Weasels Wallets", status: 0)
-        merchant_4 = Merchant.create!(name: "Donkeys Doorknobs ", status: 0)
+        merchant_1 = Merchant.create!(name: "Mans Machines", status: 1, id: 500)
+        merchant_2 = Merchant.create!(name: "Guys Gizzmos", status: 1, id: 501)
+        merchant_3 = Merchant.create!(name: "Weasels Wallets", status: 0, id: 502)
 
         visit admin_merchants_path
 
         within ('#enabled_merchants') do
-          expect(page).to have_content(merchant_1)
-          expect(page).to have_content(merchant_2)
+          expect(page).to have_content(merchant_1.name)
+          expect(page).to have_content(merchant_2.name)
+        end
+      
+        within ('#disabled_merchants') do
+          expect(page).to have_content(merchant_3.name)
+        end
+
+        within ('#disabled_merchant_502') do
+          click_button 'Enable'
         end
         
+        within ('#enabled_merchants') do
+          expect(page).to have_content(merchant_1.name)
+          expect(page).to have_content(merchant_2.name)
+          expect(page).to have_content(merchant_3.name)
+        end
+
+        within ('#enabled_merchant_502') do
+          click_button 'Disable'
+        end
+
         within ('#disabled_merchants') do
-          expect(page).to have_content(merchant_3)
-          expect(page).to have_content(merchant_4)
+          expect(page).to have_content(merchant_3.name)
         end
 
       end
