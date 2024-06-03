2985b7a531a162fc97888658d003fb6c07fb2bc7
Lance Nelson <lancen04@gmail.com>
2024-06-03 13:50:47 -0400

    corrected items controller by adding a merchant instance variable

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -3,9 +3,10 @@ require "rails_helper"
 RSpec.describe "Merchant Show Page" do
   before(:each) do
     @merchant = create(:merchant)
+    @merchant2 = create(:merchant)
     @item = @merchant.items.create!(name: "Candle", description: "Pine Scented", unit_price: 1000)
     @item2 = @merchant.items.create!(name: "Soap", description: "Fresh Spring", unit_price: 1500)
-    @item3 = @merchant.items.create!(name: "Coaster", description: "Mahogany", unit_price: 500)
+    @item3 = @merchant2.items.create!(name: "Coaster", description: "Mahogany", unit_price: 500)
   end
 # User Story 6 - Merchant Items Index Page
 # As a merchant,
@@ -16,11 +17,11 @@ RSpec.describe "Merchant Show Page" do
     describe "When I visit the merchant items index" do
       it "I see a list of only my items names" do
         visit "/merchants/#{@merchant.id}/items"
-  
+        
         expect(page).to have_content("My Items")
         expect(page).to have_content(@item.name)
         expect(page).to have_content(@item2.name)
-        expect(page).to have_content(@item3.name)
+        expect(page).to_not have_content(@item3.name)
       end
     end
   end
