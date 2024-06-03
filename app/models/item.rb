525f69409e35ce36a8922c11e6f2bb7b0f311a60
Lance Nelson <lancen04@gmail.com>
2024-06-03 13:35:24 -0400

    corrected item show page view by using number_to_currency method instead of the custom method I had built

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,8 +4,4 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items
   has_many :customers, through: :invoices
   has_many :transactions, through: :invoices
-
-  def to_dollars
-    "$" + sprintf('%.2f', self.unit_price.to_f / 100)
-  end
 end
