cef0b30d66041c220c72189fd57bd5a4d515a56d
Jan Lehigh <janlehigh461437@gmail.com>
2024-06-03 19:27:26 -0600

    Add new actions for create and new in the controller.

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -18,6 +18,28 @@ class Admin::MerchantsController < ApplicationController
       @merchant.disabled!
       notice = 'Merchant has been disabled.'
     end
-    redirect_back fallback_location: admin_merchant_path, notice: notice
+    redirect_back fallback_location: admin_merchants_path, notice: notice
+  end
+
+  def new
+    @merchant = Merchant.new
+  end
+
+  def create
+    @merchant = Merchant.new(merchant_params)
+    @merchant.status = :disabled
+
+    if @merchant.save
+      redirect_to admin_merchants_path, notice: 'Merchant was successfully created.'
+    else
+      flash.now[:alert] = 'Please fill out all the required fields.'
+      render :new
+    end
+  end
+
+  private
+
+  def merchant_params
+    params.require(:merchant).permit(:name)
   end
 end
