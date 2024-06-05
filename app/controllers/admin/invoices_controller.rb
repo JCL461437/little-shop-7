609a48d9d9bfec9b29fbc9368bbda3ce80fae9d9
Garrett Bowman <74687494+GBowman1@users.noreply.github.com>
2024-06-04 20:55:22 -0500

    Update invoices_controller.rb
    Removed unneeded line from a rabbit hole i went down

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -9,7 +9,6 @@ class Admin::InvoicesController < ApplicationController
 
   def update
     invoice = Invoice.find(params[:id])
-    invoice_params[:status] = invoice_params[:status]
     invoice.update(invoice_params)
     redirect_to admin_invoice_path(invoice)
   end
