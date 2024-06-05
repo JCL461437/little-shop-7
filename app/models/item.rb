49f6e5c577795b503743a7e64750306b4b19542a
Garrett Bowman <garrettbowman248@gmail.com>
2024-06-05 14:03:43 -0500

    Added top 5 item method

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,18 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items
   has_many :customers, through: :invoices
   has_many :transactions, through: :invoices
+
+  def self.top_five_items
+    joins(invoices: :transactions)
+    .where(transactions: {result: 'success'})
+    .select('items.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue')
+    .group(:id)
+    .order('revenue DESC')
+    .limit(5)
+  end
+# Top 5 items by revenue
+# call @merchant.items.top_five_items.each do |item|
+# item.name
+# item.revenue
+
 end
